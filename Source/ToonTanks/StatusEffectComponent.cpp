50840cdb45592960f9e575dfb602704615706e2b
Daniel Moss <danielsmoss96@gmail.com>
2025-02-23 23:22:34 +0000

    Begin implementation of status effects

--- Source/ToonTanks/StatusEffectComponent.cpp
+++ Source/ToonTanks/StatusEffectComponent.cpp
@@ -3,32 +3,57 @@
 
 #include "StatusEffectComponent.h"
 
-// Sets default values for this component's properties
 UStatusEffectComponent::UStatusEffectComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
-
-// Called when the game starts
 void UStatusEffectComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
+	Effects.Add(EStatusEffect::Slowed, 0);
+	Effects.Add(EStatusEffect::Stunned, 0);
+	Effects.Add(EStatusEffect::Wet, 0);
+	Effects.Add(EStatusEffect::Frozen, 0);
+	Effects.Add(EStatusEffect::Burning, 0);
+	Effects.Add(EStatusEffect::Poisoned, 0);
 }
 
+void UStatusEffectComponent::Init()
+{
+	ActiveEffects = EStatusEffect::None;
+}
 
-// Called every frame
 void UStatusEffectComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UpdateStatusEffects(DeltaTime);
 }
 
+void UStatusEffectComponent::UpdateStatusEffects(float DeltaTime)
+{
+	for (auto& kvp : Effects)
+	{
+		float& value = kvp.Value;
+		value -= DeltaTime;
+
+		if (value <= 0)
+		{
+			value = 0;
+			ActiveEffects &= ~kvp.Key;
+		}
+
+		if (value > 0)
+		{
+			ActiveEffects |= kvp.Key;
+		}
+	}
+}
+
+void UStatusEffectComponent::AddStatusEffect(EStatusEffect Effect, float Amount)
+{
+	float* amount = Effects.Find(Effect);
+	*amount += Amount;
+}
--- Source/ToonTanks/StatusEffectComponent.h
+++ Source/ToonTanks/StatusEffectComponent.h
@@ -6,6 +6,20 @@
 #include "Components/ActorComponent.h"
 #include "StatusEffectComponent.generated.h"
 
+UENUM(BlueprintType, meta = (Bitflags))
+enum class EStatusEffect : uint8
+{
+	None = 0 UMETA(DisplayName = "None"),
+	Slowed = 1 << 1 UMETA(DisplayName = "Slowed"),
+	Stunned = 1 << 2 UMETA(DisplayName = "Stunned"),
+	Wet = 1 << 3 UMETA(DisplayName = "Wet"),
+	Frozen = 1 << 4 UMETA(DisplayName = "Frozen"),
+	Burning = 1 << 5 UMETA(DisplayName = "Burning"),
+	Poisoned = 1 << 6 UMETA(DisplayName = "Poisoned"),
+};
+
+ENUM_CLASS_FLAGS(EStatusEffect);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TOONTANKS_API UStatusEffectComponent : public UActorComponent
 {
@@ -14,15 +28,23 @@ class TOONTANKS_API UStatusEffectComponent : public UActorComponent
 public:	
 	UStatusEffectComponent();
 
+	void Init();
+
+	UFUNCTION(BlueprintCallable)
+	void AddStatusEffect(EStatusEffect Effect, float Amount);
+	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-	//Make an enum to select a status effect type
-	//Make a map of status effect type to how long it is active for
-	//Loop it per some longer tick time (0.2?) and apply effects as needed
+	void UpdateStatusEffects(float DeltaTime);
+	
 	//Put getter methods on EntityStats and use those instead of the pure properties
 	//Apply multipliers to entity stats in those getters based on active effects
+
+	TMap<EStatusEffect, float> Effects;
+
+	UPROPERTY(VisibleAnywhere, Category = "StatusEffects")
+	EStatusEffect ActiveEffects;
 
 public:	
 	// Called every frame
